6fd87c65b43376a61d0941269e6a31aff555aea8
kibra <62299912+kibradev@users.noreply.github.com>
2021-07-16 15:59:37 +0300

    Update kibra-emlak.sql

--- kibra-emlak/kibra-emlak.sql
+++ kibra-emlak/kibra-emlak.sql
@@ -9,3 +9,12 @@ INSERT INTO `kibra_emlak` (`id`, `sifre`) VALUES
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`, `type`, `unique`, `description`, `image`, `shouldClose`, `combinable`) VALUES
 ('emlak_key', 'Emlakçı Depo Anahtar', 0.01, 0, 1, 'item', 'false', 'Emlakçı Depo Anahtarı', 'motel_key.png', '0', NULL)
 ;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('emlak', 'Emlak')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('emlak',1,'Emlak','Emlakçı',12,'{}','{}')
+
+;
